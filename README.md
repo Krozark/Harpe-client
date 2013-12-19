3d04d06ff2e168fb530604244ecdd2fade5387ad
Krozark <maxime.barbier1991@gmail.com>
2013-12-19 16:05:15 +0100

    add requirerment

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Harpe
 =====
 
 Soft to make MS/MS data analysis
+
+Requirement
+===========
+
+libharpe-sort : https://github.com/Krozark/Harpe-sort
--- harpe-client/src/client/main.cpp
+++ harpe-client/src/client/main.cpp
@@ -1,37 +1,15 @@
+#include <client/functions.hpp>
 ///data base
-#include <Socket/client/Client.hpp>
 #include <Socket/Config.hpp>
-#include <Socket/FuncWrapper.hpp>
-#include <Socket/SocketSerialized.hpp>
+#include <Socket/client/Client.hpp>
+
 
 #define SERVER_HOST 1
 #define SERVER_PORT 2
 
-
-namespace ntw
-{
-    int dispatch(int id,SocketSerialized& request){}
-}
-
-void clientWaitForWork(ntw::SocketSerialized& sock)
-{
-    ntw::FuncWrapper::cli::send(sock,3);
-
-}
-
-enum ERRORS {
-    OK = 0,
-    INPUT_NOT_VALID = 1,
-    EMPTY_INPUT = 2,
-    PK_ERROR = 3,
-    EMPTY_DATA_SEND = 4,
-    TIMEOUT = 5,
-    STOP = 100
-};
-
 int main(int argc,char* argv[])
 {
-    if(argc < SERVER_HOST)
+    if(argc < SERVER_HOST+1)
     {
         std::cout<<"Usage are: "<<argv[0]<<" <server-host> [server-port]"<<std::endl;
         return 1;
@@ -45,35 +23,15 @@ int main(int argc,char* argv[])
     ntw::Config::default_timeout = 5.f;
     ntw::Config::broadcast = false;
 
+    if(not ini_context("./sort.so"))
+        return 0;
+
     ntw::cli::Client client;
     client.connect(argv[SERVER_HOST],ntw::Config::port_server);
 
-    bool run=true;
 
-    while (run)
-    {
-        client.call(clientWaitForWork);
-        switch(client.request_sock.getStatus())
-        {
-            case ERRORS::OK :
-            {
-                ///\todo start analyse
-                /// ask new task
-            }break;
-            case ERRORS::TIMEOUT :
-            {
-                /// ask new task
-            }break;
-            case ERRORS::STOP :
-            {
-                run = false;
-            }break;
-            default :
-            {
-                /// server error???
-            }break;
-        }
-    }
-
+    run(client);
+    clean_context();
+    
     return 0;
 }
