aaea0cc7a0a7fb584fc27352d16b4d9b80707f48
Krozark <maxime.barbier1991@gmail.com>
2014-01-13 16:24:15 +0100

    add models

--- harpe-client/include/client/models.hpp
+++ harpe-client/include/client/models.hpp
@@ -4,21 +4,56 @@
 #include <string>
 #include <iostream>
 
+#include <list>
+
 namespace ntw
 {
     class Serializer;
 }
 
+/****************************
+ ****   AA ******************
+ ***************************/
+
+class AA
+{
+    public:
+        int pk;
+        std::string slug;
+        float mass;
+
+        friend ntw::Serializer& operator>>(ntw::Serializer& stream,AA& self);
+        friend std::ostream& operator<<(std::ostream& stream,AA& self);
+};
+
+/*****************************
+ ****** AnalyseMgf ***********
+ ****************************/
+
+class AnalyseMgf
+{
+    public:
+        int pk;
+        std::list<AA> AAs;
+        
+        friend ntw::Serializer& operator>>(ntw::Serializer& stream,AnalyseMgf& self);
+        friend std::ostream& operator<<(std::ostream& stream,AnalyseMgf& self);
+
+};
+
+/***********************************
+ ********** AnalysePeptide ********
+ * ********************************/
+
 class AnalysePeptide
 {
     public:
         int pk;
-        int analyse_fk;
         std::string mgf_part;
+        AnalyseMgf analyse;
 
         friend ntw::Serializer& operator>>(ntw::Serializer& stream,AnalysePeptide& self);
         friend std::ostream& operator<<(std::ostream& stream,AnalysePeptide& self);
 };
-
 
 #endif
--- harpe-client/src/client/models.cpp
+++ harpe-client/src/client/models.cpp
@@ -2,14 +2,68 @@
 
 #include <Socket/Serializer.hpp>
 
+/****************************
+ ****   AA ******************
+ ***************************/
+
+ntw::Serializer& operator>>(ntw::Serializer& stream,AA& self)
+{
+    stream>>self.pk
+        >>self.slug
+        >>self.mass;
+    return stream;
+}
+
+std::ostream& operator<<(std::ostream& stream, AA& self)
+{
+    stream<<"pk:"<<self.pk
+        <<", slug "<<self.slug
+        <<", mass: "<<self.mass;
+    return stream;
+};
+
+/*****************************
+ ****** AnalyseMgf ***********
+ ****************************/
+
+ntw::Serializer& operator>>(ntw::Serializer& stream,AnalyseMgf& self)
+{
+    unsigned int size;
+    stream>>self.pk
+        >>size;
+    for(unsigned int i=0;i<size;++i)
+    {
+        AA tmp;
+        stream>>tmp;
+        self.AAs.emplace_back(std::move(tmp));
+    }
+    return stream;
+};
+
+std::ostream& operator<<(std::ostream& stream, AnalyseMgf& self)
+{
+    stream<<"pk:"<<self.pk
+        <<", size: "<<self.AAs.size();
+    for(AA& aa : self.AAs)
+        stream<<", AA: "<<aa;
+    return stream;
+};
+
+/***********************************
+ ********** AnalysePeptide ********
+ * ********************************/
 ntw::Serializer& operator>>(ntw::Serializer& stream, AnalysePeptide& self)
 {
-    stream>>self.pk>>self.analyse_fk>>self.mgf_part;
+    stream>>self.pk
+        >>self.mgf_part
+        >>self.analyse;
     return stream;
 };
 
 std::ostream& operator<<(std::ostream& stream, AnalysePeptide& self)
 {
-    stream<<"pk:"<<self.pk<<", fk: "<<self.analyse_fk<<", mgf: "<<self.mgf_part;
+    stream<<"pk:"<<self.pk
+        <<", mgf: "<<self.mgf_part
+        <<", analyse: "<<self.analyse;
     return stream;
 };
