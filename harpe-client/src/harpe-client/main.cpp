bba03a90d76358d83924b29c414f9ebfb696a359
Krozark <maxime.barbier1991@gmail.com>
2014-03-21 17:50:30 +0100

    get server information from website

--- harpe-client/src/harpe-client/main.cpp
+++ harpe-client/src/harpe-client/main.cpp
@@ -5,30 +5,59 @@
 #include <Socket/client/Client.hpp>
 
 
-#define SERVER_HOST 1
-#define SERVER_PORT 2
+#define WEBSITE_HOST 1
+#define WEBSITE_PORT 2
+
+int register_to_website(char host[],int port)
+{
+    int status = 0;
+
+    std::cout<<"[Loggin to website] "<<host<<":"<<port<<std::endl;
+    ntw::Socket website_sock(ntw::Socket::Dommaine::IP,ntw::Socket::Type::TCP);
+    website_sock.connect(host,port);
+
+    std::string msg;
+    msg += "GET /get/ ";
+    msg += "HTTP/1.1\r\nHOST: ";
+    msg += host;
+    msg += "\r\n\r\n";
+
+    char buffer[1024];
+    website_sock.send(msg.c_str(),msg.size());
+
+    int recv;
+    float version = 0;
+    std::string page;
+    while((recv = website_sock.receive(buffer,1024))>0)
+    {
+        page.append(buffer,recv);
+    }
+    std::cout<<page<<std::endl<<std::flush;
+
+    return status;
+}
+
 
 int main(int argc,char* argv[])
 {
     std::cout<<"===\nHarpe client\nversion:"<<MAJOR_VERSION<<"."<<MINOR_VERSION<<"."<<PATCH_VERSION<<"\n===\n"<<std::endl;
 
-    if(argc < SERVER_HOST+1)
+    if(argc < WEBSITE_PORT+1)
     {
-        std::cout<<"Usage are: "<<argv[0]<<" <server-host> [server-port]"<<std::endl;
+        std::cout<<"Usage are: "<<argv[0]<<" <website-host> <website-port>"<<std::endl;
         return 1;
     }
-    //// inti config
-    if (argc >= SERVER_PORT+1)
-        ntw::Config::port_server = atoi(argv[SERVER_PORT]);
-    else
-        ntw::Config::port_server = 3987;
+    
+    register_to_website(argv[WEBSITE_HOST],atoi(argv[WEBSITE_PORT]));
 
+    //// inti config
+    ntw::Config::port_server = atoi(argv[WEBSITE_PORT]);
 
     if(not ini_context("./harpe-sort.so"))
         return 0;
 
     ntw::cli::Client client;
-    client.connect(argv[SERVER_HOST],ntw::Config::port_server);
+    client.connect(argv[WEBSITE_PORT],ntw::Config::port_server);
 
 
     run(client);
