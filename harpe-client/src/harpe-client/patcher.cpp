0f2eb7b97f2bb799491ccd478f29a7985a0dd93f
Krozark <maxime.barbier1991@gmail.com>
2014-07-29 10:10:54 +0200

    add startup for windows

--- harpe-client/src/harpe-client/patcher.cpp
+++ harpe-client/src/harpe-client/patcher.cpp
@@ -4,6 +4,9 @@
 #include <utils/string.hpp>
 
 #if _WIN32
+#include <QCoreApplication>
+#include <QSettings>
+
 extern "C" {
     int _get_output_format( void ){ return 0; } //to avoid linker error on windows
 }
@@ -13,6 +16,14 @@ extern "C" {
 int main(int argc,char* argv[])
 {
     QApplication App(argc, argv);
+
+     #if _WIN32
+    QSettings settings("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", QSettings::NativeFormat);
+    QString value = QCoreApplication::applicationFilePath(); //get absolute path of running exe
+    value.replace("/","\\");
+    settings.setValue("harpe-client-patcher", value);
+    #endif
+
 
     patcher::Patcher patcher("harpe-client");
     patcher.show();
