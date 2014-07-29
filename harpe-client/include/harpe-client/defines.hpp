69537afc0c0ebc5d6469a21df2237a65b82d7d44
Krozark <maxime.barbier1991@gmail.com>
2014-07-29 13:45:39 +0200

    update version

--- harpe-client/include/harpe-client/defines.hpp
+++ harpe-client/include/harpe-client/defines.hpp
@@ -5,7 +5,7 @@
 
 #define MAJOR_VERSION 0 //major version number (0..9999)
 #define MINOR_VERSION 4 //minor version number (0..99)
-#define PATCH_VERSION 5 //patch version number (0..99)
+#define PATCH_VERSION 6 //patch version number (0..99)
 
 #define VERSION ((MAJOR_VERSION*100*100)+(MINOR_VERSION*100)+PATCH_VERSION)
 #define LOG(sock,func,msg) std::cerr<<"[id:"<<sock.id()<<"] "<< func<<"()"<<" => "<<msg <<std::endl;
